6120fa3c984163a4f6b12e29290c23c81fb816a7
Nathan Rathe <nprathe10@gmail.com>
2019-07-24 18:40:27 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,9 +13,19 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    Appointment.new(self, patient, date)
+    Appointment.new(patient, self, date)
   end
 
+  def appointments
+    Appointment.all.select do |a|
+      a.doctor == self
+    end
+  end
 
+  def patients
+    appointments.collect do |a|
+      a.patients
+    end
+  end
 
 end
