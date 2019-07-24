4ce5e16af365786e173879a5975006ba9ce20946
Nathan Rathe <nprathe10@gmail.com>
2019-07-24 17:18:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,19 @@
 class Artist
 
-  attr_accessor
+  attr_accessor :name, :artist, :genre
   @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def all
-    @@all << self
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
   end
 
 end
